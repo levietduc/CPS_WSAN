e2dfa34dab26ef7dcc2d979c63ba66a6b8c6cb9d
Le Viet Duc <levietduc@gmail.com>
2019-12-16 05:04:58 +0100

    Tutorial 04 - nRF52 firmware version 04
    This is the example for the final demo.
    Added reading real sensor data, temperature, pressure, and humidity. Compatibles with myWSAN-debug4-RealSensorData.apk.
    Still couldn't fix bug: 2 cluster heads may have a conflict on connecting to a same Thingy. This bug is from the orignial code of provided by Nordic, multilink.

---
+++ nrf52Tutorial/ble_aggregator/pca10040/s132/ses/Output/ble_aggregator_pca10040_s132 Release/Obj/ses_startup_nrf52.d
@@ -0,0 +1,2 @@
+Output/ble_aggregator_pca10040_s132\ Release/Obj/ses_startup_nrf52.o: \
+ /Users/levietduc/CPS2019/nRF5_SDK/modules/nrfx/mdk/ses_startup_nrf52.s
---
+++ nrf52Tutorial/ble_aggregator/pca10040/s132/ses/Output/ble_aggregator_pca10040_s132 Release/Obj/system_nrf52.d
@@ -0,0 +1,16 @@
+Output/ble_aggregator_pca10040_s132\ Release/Obj/system_nrf52.o: \
+ /Users/levietduc/CPS2019/nRF5_SDK/modules/nrfx/mdk/system_nrf52.c \
+ /Applications/SEGGER\ Embedded\ Studio\ for\ ARM\ 4.30a/include/stdint.h \
+ /Applications/SEGGER\ Embedded\ Studio\ for\ ARM\ 4.30a/include/stdbool.h \
+ /Users/levietduc/CPS2019/nRF5_SDK/modules/nrfx/mdk/nrf.h \
+ /Users/levietduc/CPS2019/nRF5_SDK/modules/nrfx/mdk/nrf52.h \
+ ../../../../../../../components/toolchain/cmsis/include/core_cm4.h \
+ ../../../../../../../components/toolchain/cmsis/include/core_cmInstr.h \
+ ../../../../../../../components/toolchain/cmsis/include/cmsis_gcc.h \
+ ../../../../../../../components/toolchain/cmsis/include/core_cmFunc.h \
+ ../../../../../../../components/toolchain/cmsis/include/core_cmSimd.h \
+ /Users/levietduc/CPS2019/nRF5_SDK/modules/nrfx/mdk/system_nrf52.h \
+ /Users/levietduc/CPS2019/nRF5_SDK/modules/nrfx/mdk/nrf52_bitfields.h \
+ /Users/levietduc/CPS2019/nRF5_SDK/modules/nrfx/mdk/nrf51_to_nrf52.h \
+ /Users/levietduc/CPS2019/nRF5_SDK/modules/nrfx/mdk/nrf52_name_change.h \
+ /Users/levietduc/CPS2019/nRF5_SDK/modules/nrfx/mdk/compiler_abstraction.h
